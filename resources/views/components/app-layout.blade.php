74157afbfc1d3ba7ce0c965ea065cd72afc9b879
Daniel Alberto Salazar Erazo <dsalazar1982@yahoo.com>
2025-04-26 10:39:50 -0500

    Se crea una plantilla
    Se crea una plantilla por medio de componentes anonimos y se guarda en
    este componente todo el contenido que sera fijo para las vistas.

---
+++ resources/views/components/app-layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel 11</title>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <!-- {{-- Font Awesome --}} -->
+    <!-- {{-- tipografia --}} -->
+</head>
+
+<body>
+
+<header></header>
+
+{{ $slot }}
+
+
+<footer></footer>
+
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 11</title>
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
-</head>
-
-<body>
+<x-app-layout>
     <div class="max-w-4xl mx-auto px-4">
         <!-- <h1>Bienvenido a la pagina principal</h1> -->
-         <x-alert2 type='info'>
+        <x-alert2 type='info'>
             <x-slot name='title'>
                 TITULO:
             </x-slot>
             Contenido de la alerta.
-         </x-alert2>
+        </x-alert2>
 
-         <p>HOLA MUNDO!</p>
+        <p>HOLA MUNDO CAMBIANDO!</p>
     </div>
-</body>
-
-</html>
+</x-app-layout>
