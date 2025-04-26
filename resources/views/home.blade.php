c5e75a5c5a834642dc3dbe8f675b07c2c33163e6
Daniel Alberto Salazar Erazo <dsalazar1982@yahoo.com>
2025-04-26 09:40:59 -0500

    Se agrega componentes a la vista home.
    Se crea un componente anonimo llamado alert asi como un componente de
    clase denominado Alert2.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 11</title>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 </head>
+
 <body>
-    <h1>Bienvenido a la pagina principal</h1>
+    <div class="max-w-4xl mx-auto px-4">
+        <!-- <h1>Bienvenido a la pagina principal</h1> -->
+         <x-alert2 type='info'>
+            <x-slot name='title'>
+                TITULO:
+            </x-slot>
+            Contenido de la alerta.
+         </x-alert2>
+
+         <p>HOLA MUNDO!</p>
+    </div>
 </body>
+
 </html>
