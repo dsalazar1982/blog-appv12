91a9be810bd0348e5bd9d7c8445365164ac6c47d
Daniel Alberto Salazar Erazo <dsalazar1982@yahoo.com>
2025-05-19 15:35:53 -0500

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 <x-app-layout>
     <div class="max-w-4xl mx-auto px-4">
         <!-- <h1>Bienvenido a la pagina principal</h1> -->
-        <x-alert2 type='info'>
+        <!--<x-alert2 type='info'>
             <x-slot name='title'>
                 TITULO:
             </x-slot>
             Contenido de la alerta.
-        </x-alert2>
+        </x-alert2> -->
+
+        <h1 style="text-align: center;">ESTA ES UNA PAGNA DE INICIO</h1>
 
         <p>HOLA MUNDO CAMBIANDO!</p>
     </div>
